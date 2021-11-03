b55c6b8f8d317ccd142f0a8b15739afa8d3a41cf
Dong xiannan <443852256@qq.com>
2021-11-03 17:56:01 +0800

    工具

--- src/Commands/ModelMakeCommand.php
+++ src/Commands/ModelMakeCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace dongxiannan\entities\Commands;
+namespace Dongxiannan\Entities\Commands;
 
 use \Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -27,9 +27,13 @@ class ModelMakeCommand extends Command
         $database = $this->option('database');
         $update = $this->option('update');
         $namespace = $this->getNamespace($module);
-        $tableName = $this->humpToLine($model);//表明
+        $tableName = $this->humpToLine($model);//表名
         //创建模块
         $this->createModel($model, $module, $database, $update, $namespace, $tableName);
+        //创建服务
+        $this->createService($model, $module, $update);
+        //创建仓库
+        $this->createRepository($model, $module, $update);
     }
 
    /*
@@ -48,6 +52,11 @@ class ModelMakeCommand extends Command
             $columns =  DB::connection($database)->select('SHOW COLUMNS FROM `' . $tableName . '`');
         }else{
             $columns =  DB::select('SHOW COLUMNS FROM `' . $tableName . '`');
+        }
+        if($database){
+            $connection = "protected \$connection = '".$database."';".PHP_EOL;
+        }else{
+            $connection = '';
         }
 
         $columnsIde = '';
@@ -70,6 +79,7 @@ class ModelMakeCommand extends Command
         $columnsIde.=' *';
         $template_temp = $template;
         $source = str_replace('{{class_name}}', $className, $template_temp);
+        $source = str_replace('{{connection}}', $connection, $source);
         $source = str_replace('{{table_name}}', $tableName, $source);
         $source = str_replace('{{namespace}}', $namespace, $source);
         $source = str_replace('{{ide_property}}', $columnsIde, $source);
@@ -92,6 +102,65 @@ class ModelMakeCommand extends Command
         }
     }
 
+    /*
+     * 创建服务
+     */
+    protected function createService($model, $module, $update){
+        $className = $model.'Service';
+        //获取模板文件
+        $template = file_get_contents(dirname(__FILE__) . '/stubs/service.stub');
+        $source = str_replace('{{model}}', $model, $template);
+        $source = str_replace('{{model_lower}}', lcfirst($model), $source);
+        $source = str_replace('{{module}}', $module, $source);
+
+        $modelPath = 'Modules/'.$module.'/Services';
+        $filePath ='Modules/'.$module.'/Services/'.$className.'.php';
+
+        //写入文件
+        if (!is_dir($modelPath)) {
+            $res = mkdir($modelPath, 0755, true);
+            if (!$res) $this->error('目录' . $modelPath . ' 无法写入文件,创建' . $className . ' 失败');
+        }
+
+        if(File::exists($filePath) && !$update){
+            return $this->info($className . ' 类已经存在');
+        }
+        if (file_put_contents($filePath, $source)) {
+            $this->info($className . ' 类添加成功');
+        } else {
+            $this->error($className . ' 类写入失败');
+        }
+    }
+
+    /*
+     * 创建仓库
+     */
+    protected function createRepository($model, $module, $update){
+        $className = $model.'Repository';
+        //获取模板文件
+        $template = file_get_contents(dirname(__FILE__) . '/stubs/repository.stub');
+        $source = str_replace('{{model}}', $model, $template);
+        $source = str_replace('{{model_lower}}', lcfirst($model), $source);
+        $source = str_replace('{{module}}', $module, $source);
+
+        $modelPath = 'Modules/'.$module.'/Repositories';
+        $filePath ='Modules/'.$module.'/Repositories/'.$className.'.php';
+
+        //写入文件
+        if (!is_dir($modelPath)) {
+            $res = mkdir($modelPath, 0755, true);
+            if (!$res) $this->error('目录' . $modelPath . ' 无法写入文件,创建' . $className . ' 失败');
+        }
+
+        if(File::exists($filePath) && !$update){
+            return $this->info($className . ' 类已经存在');
+        }
+        if (file_put_contents($filePath, $source)) {
+            $this->info($className . ' 类添加成功');
+        } else {
+            $this->error($className . ' 类写入失败');
+        }
+    }
     /*
      * 获取命名空间
      */
